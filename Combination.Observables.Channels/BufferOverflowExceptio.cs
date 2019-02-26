89719e183028fe1e824889ef7990c9b270208fee
Rickard Lyrenius <rly@combination.se>
2019-02-26 17:17:07 +0100

    Initial commit

---
+++ Combination.Observables.Channels/BufferOverflowExceptio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Combination.Observables.Channels
+{
+    [Serializable]
+    public sealed class BufferOverflowException : Exception
+    {
+        public BufferOverflowException(string message) : base(message)
+        {
+        }
+        private BufferOverflowException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Combination.Observables.Channels/ObservableChannelReaderExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace Combination.Observables.Channels
+{
+    public static class ObservableChannelReaderExtensions
+    {
+        private const int defaultCapacity = 8;
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, UnboundedChannelOptions unboundedChannelOptions, bool failOnDrop = true)
+        {
+            return AsChannelReader(topic, unboundedChannelOptions, CancellationToken.None, failOnDrop);
+        }
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, UnboundedChannelOptions unboundedChannelOptions, CancellationToken cancellationToken, bool failOnDrop = true)
+        {
+            var channel = Channel.CreateUnbounded<T>(unboundedChannelOptions);
+            return AsChannelReaderInternal(topic, channel, cancellationToken, failOnDrop);
+        }
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, int capacity, bool failOnDrop = true)
+        {
+            return AsChannelReader(topic, capacity, CancellationToken.None, failOnDrop);
+        }
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, BoundedChannelOptions boundedChannelOptions, bool failOnDrop = true)
+        {
+            return AsChannelReader(topic, boundedChannelOptions, CancellationToken.None, failOnDrop);
+        }
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, CancellationToken cancellationToken, bool failOnDrop = true)
+        {
+            return AsChannelReader(topic, defaultCapacity, cancellationToken, failOnDrop);
+        }
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, bool failOnDrop = true)
+        {
+            return AsChannelReader(topic, defaultCapacity, CancellationToken.None, failOnDrop);
+        }
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, int capacity, CancellationToken cancellationToken, bool failOnDrop = true)
+        {
+            var channel = Channel.CreateBounded<T>(capacity);
+            return AsChannelReaderInternal(topic, channel, cancellationToken, failOnDrop);
+        }
+
+        public static ChannelReader<T> AsChannelReader<T>(this IObservable<T> topic, BoundedChannelOptions boundedChannelOptions, CancellationToken cancellationToken, bool failOnDrop = true)
+        {
+            var channel = Channel.CreateBounded<T>(boundedChannelOptions);
+            return AsChannelReaderInternal(topic, channel, cancellationToken, failOnDrop);
+        }
+
+        private static ChannelReader<T> AsChannelReaderInternal<T>(IObservable<T> topic, Channel<T> channel, CancellationToken cancellationToken, bool failOnDrop)
+        {
+            var disposable = topic.Subscribe(value =>
+                {
+                    if (!channel.Writer.TryWrite(value) && failOnDrop)
+                    {
+                        try
+                        {
+                            channel.Writer.Complete(new BufferOverflowException("ChannelReader buffer overflow"));
+                        }
+                        catch
+                        {
+                            // Error in error handling
+                        }
+                    }
+                },
+                error => channel.Writer.TryComplete(error),
+                () => channel.Writer.TryComplete());
+
+            var connectionAbortedRegistration = cancellationToken.Register(() =>
+            {
+                channel.Writer.TryComplete();
+
+                // Read lingering items in the Reader to make sure it completes
+                while (channel.Reader.TryRead(out var _))
+                {
+                    // Do nothing
+                }
+            });
+
+            channel.Reader.Completion.ContinueWith(task =>
+            {
+                disposable.Dispose();
+                connectionAbortedRegistration.Dispose();
+            });
+
+            return channel.Reader;
+        }
+    }
+}
